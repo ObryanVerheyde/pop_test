a157a8c6ce3cb5b6c2dfce44ab8f153ce17916ca
Obryan Verheyde <verheyde.obryan@gmail.com>
2017-05-19 11:00:37 +0200

    add routes users

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -17,14 +17,6 @@ namespace App\Controller;
 use Cake\Controller\Controller;
 use Cake\Event\Event;
 
-namespace App\Controller;
-
-class AppController extends \Cake\Controller\Controller
-{
-    use \Crud\Controller\ControllerTrait;
-
-}
-
 /**
  * Application Controller
  *
